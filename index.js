9f4d58c209243c8167e74789c07a5d253991f077
hugohp19 <hp19@hotmail.com>
2020-10-11 22:06:47 -0400

    added instructions, validations and guess whole word feature

--- index.js
+++ index.js
@@ -12,9 +12,18 @@ console.log('***                                / \\  __|    ***');
 console.log('*************************************************');
 console.log('-------------------------------------------------\n\n\n');
 
-let mediumWordsArray = ['even', 'pizza', 'water', 'sixty', 'angel', 'music', 'fifty', 'party', 'woman', 'sugar', 'amber'];
-let hardWordsArray = ['abnegation', 'abstruse', 'archetypal', 'bombastic', 'camaraderie', 'circumlocution', 'multifarious', 'pellucid', 'punctilious', 'sanctimonious'];
-let impossibleWordsArray = ['supercalifragilisticexpialidocious', 'pseudopseudohypoparathyroidism', 'antidisestablishmentarianism', 'thyroparathyroidectomized', 'dichlorodifluoromethane', 'incomprehensibilitie'];
+console.log('Instructions:');
+console.log('1.- Choose a difficulty by typing: Medium, Hard or Impossible');
+console.log('2.- The app will think of a word. Your job is to guess the word by typing letter by letter or by typing the whole word.');
+console.log('       a.- If you want to guess by letter, just type the letter that you want to guess first');
+console.log('       b.- If you want to guess by word, type in the whole word. More than one letter will count as if you are trying to guess the whole word.\n          You only have one try for the whole word');
+console.log('3.- If the letter you guess is in the word, the app will show you the letter and the position within the word');
+console.log('4.- If the letter is not in the word, that will count as a miss.')
+console.log('5.- If you have 6 missed, you lose. Correct letters do not count as misses.\n\n');
+
+let mediumWordsArray = ['even', 'pizza', 'water', 'sixty', 'angel', 'music', 'fifty', 'party', 'woman', 'sugar', 'amber', 'believe', 'lyric', 'catch', 'said'];
+let hardWordsArray = ['abnegation', 'abstruse', 'archetypal', 'bombastic', 'camaraderie', 'circumlocution', 'multifarious', 'pellucid', 'punctilious', 'sanctimonious', 'expectation', 'hilarious', 'scarce', 'unfamiliar', 'equivalent'];
+let impossibleWordsArray = ['supercalifragilisticexpialidocious', 'pseudopseudohypoparathyroidism', 'antidisestablishmentarianism', 'thyroparathyroidectomized', 'dichlorodifluoromethane', 'incomprehensibilitie', 'baccalaureate', 'extemporaneous', 'nomenclature'];
 let placeholder= [];
 let misteryWord;
 let maxTries = 6;
@@ -22,7 +31,11 @@ let letterPosition = [];
 
 function gameStarter(){
     console.log('Chose your Dificulty:')
-    let difficulty = readline.question(`type: Medium, Hard, Impossible\n`);
+    let difficulty = readline.question(`type: Medium, Hard, Impossible\n`).toLowerCase();
+    while(difficulty !== 'medium' && difficulty !== 'hard' && difficulty !== 'impossible'){
+        difficulty = readline.question(`Please type: Medium, Hard, or Impossible\n`).toLowerCase();
+        console.log(difficulty);
+    }
     console.clear();
     switch(difficulty.toLowerCase()){
         case 'medium':
@@ -61,64 +74,63 @@ function hangman(letter){
     let lettersGuessed = 0;
     let counter = 0;
     let letterArray = [];
-    for(let i = 0; i < maxTries; i++){
-        if(letterArray.includes(letter)){
-            console.log(`You already typed that letter. Letters typed ( ${letterArray})\n`);
-            i--;
-        } else {
-            let letterIncluded = findLetter(letter, misteryWord);
-            if(letterIncluded.length > 0){
-                for(let y = 0; y < letterIncluded.length; y++){
-                    // console.log(letterIncluded.length);
-                    // console.log(letterIncluded);
-                    // console.log(letter);
-                    placeholder[letterIncluded[y]] = letter;
-                    // console.log(placeholder);
-                    lettersGuessed++;
-                }
-                letterArray.push(letter);
-                console.clear();
-                console.log('✅️   Wuhuu, You got it');
-                let placeHolderString = placeholder.toString().replace(/,/g, ' ');
-                console.log(placeHolderString);
+    let playAgain;
+
+    if(letter.length > 1 && letter === misteryWord){
+        youWin();
+        playAgain = readline.question('Do you want to play again? y/n:   ');
+        playAgainFunc(playAgain);
+    }else if(letter.length > 1){
+        yourLose();
+        playAgain = readline.question('Do you want to play again? y/n:   ');
+        playAgainFunc(playAgain);
+    }else {
+        for(let i = 0; i < maxTries; i++){
+            if(letterArray.includes(letter)){
+                console.log(`You already typed that letter. Letters typed ( ${letterArray})\n`);
                 i--;
             } else {
-                console.clear();
-                letterArray.push(letter);
-                console.log(`❌️   The letter: ${letter} is not in the word`);
-                placeHolderString = placeholder.toString().replace(/,/g, ' ');
-                console.log(placeHolderString);
-                counter++;
-                visualHangman(counter);
+                let letterIncluded = findLetter(letter, misteryWord);
+                if(letterIncluded.length > 0){
+                    for(let y = 0; y < letterIncluded.length; y++){
+                        placeholder[letterIncluded[y]] = letter;
+                        lettersGuessed++;
+                    }
+                    letterArray.push(letter);
+                    console.clear();
+                    console.log('✅️   Wuhuu, You got it');
+                    let placeHolderString = placeholder.toString().replace(/,/g, ' ');
+                    console.log(placeHolderString);
+                    i--;
+                } else {
+                    console.clear();
+                    letterArray.push(letter);
+                    console.log(`❌️   The letter: ${letter} is not in the word`);
+                    placeHolderString = placeholder.toString().replace(/,/g, ' ');
+                    console.log(placeHolderString);
+                    counter++;
+                    visualHangman(counter);
+                }
+            }
+
+            if(counter >= maxTries){
+                yourLose();
+                break;
+            }else if(lettersGuessed === misteryWord.length){
+                youWin();
+                break;
+            }else{
+                letter = readline.question(`\nwhat is your next letter:    `).toLowerCase();
             }
         }
 
-        if(counter >= maxTries){
-            console.clear();
-            console.log(`\n\n💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀`);
-            console.log(`❌️                                                              ❌️`);
-            console.log(`❌️   You have reached the maximun number of misses. YOU LOSE!   ❌️`);
-            console.log(`     👀 👀 👀 The Word was: ${misteryWord}`);
-            console.log(`💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀\n\n`);
-        }else if(lettersGuessed === misteryWord.length){
-            console.log('\n\n🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩');
-            console.log('🌟 🌟 🌟 🌟                      🌟 🌟 🌟 🌟');
-            console.log('🌟 🌟 🌟 🌟    You WIN!!!!!!!!   🌟 🌟 🌟 🌟');
-            console.log('🌟 🌟 🌟 🌟                      🌟 🌟 🌟 🌟');
-            console.log('🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩\n\n');
-            break;
-        }else{
-            letter = readline.question(`\nwhat is your next letter:    `).toLowerCase();
-        }
+        playAgain = readline.question('Do you want to play again? y/n:   ');
+        playAgainFunc(playAgain);
+
     }
 
-    let playAgain = readline.question('Do you want to play again? y/n:   ');
-    if(playAgain.toLowerCase() === 'y'){
-        console.clear();
-        gameStarter();
-    } else {
-        console.log('\n********* SEE YOU SOON ***********\n');
-    }
+
+
 }
 
 
@@ -134,6 +146,35 @@ function findLetter(letter, misteryWord){
     return letterPosition;
 }
 
+function playAgainFunc(yesOrNo){
+    while(yesOrNo.toLowerCase() !== 'y' && yesOrNo.toLowerCase() !== 'n'){
+        yesOrNo = readline.question('Do you want to play again? y/n:   ');
+    }
+
+    if(yesOrNo.toLowerCase() === 'y'){
+        console.clear();
+        gameStarter();
+    } else {
+        console.log('\n********* SEE YOU SOON ***********\n');
+    }
+}
+
+function youWin(){
+    console.log('\n\n🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩');
+    console.log('🌟 🌟 🌟 🌟                      🌟 🌟 🌟 🌟');
+    console.log('🌟 🌟 🌟 🌟    You WIN!!!!!!!!   🌟 🌟 🌟 🌟');
+    console.log('🌟 🌟 🌟 🌟                      🌟 🌟 🌟 🌟');
+    console.log('🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩 🤩\n\n');
+}
+
+function yourLose(){
+    console.clear();
+    console.log(`\n\n💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀`);
+    console.log(`❌️                                                              ❌️`);
+    console.log(`❌️   You have reached the maximun number of misses. YOU LOSE!   ❌️`);
+    console.log(`     👀 👀 👀 The Word was: ${misteryWord}`);
+    console.log(`💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀\n\n`);
+}
 
 function visualHangman(number){
     switch(number){
